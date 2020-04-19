722c07e0f05ed10bc32cd9cd405c6bbeab73aac9
dubssss <33904830+dubssss@users.noreply.github.com>
2020-04-20 03:02:25 +0530

    added sendemail function, fixed session

--- app.py
+++ app.py
@@ -1,38 +1,48 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+import sendemailmsg
+app = Flask(__name__)
+app.secret_key="seckey"
 
-app=Flask(__name__)
 @app.route('/', methods=["GET"])
 def home():
     return render_template('login.html')
 
-@app.route('/uservalid',methods=["POST","GET"])
+@app.route('/uservalid', methods=["POST", "GET"])
 def uservalid():
-    username=request.form.get("username")
-    password=request.form.get("password")
-    if (username=="anubha" and password=="dubey"):
+    if 'loggedin' in session:
+        render_template('chatbot.html')
+    username = request.form.get("username")
+    password = request.form.get("password")
+    if (username == "anubha" and password == "dubey"):
+        session['loggedin']=username
         return render_template('chatbot.html')
     else:
-        return "<h1>Incorect Credentials</h1>"
+        return "<h1>Incorrect Credentials</h1>"
 
-@app.route('/processreply', methods=["POST","GET"])
+@app.route('/processreply', methods=["POST", "GET"])
 def processreply():
     jobject = request.json
-    msg=(jobject['msg'])
-    msg=msg.upper().rstrip()
-    botreply=str
+    msg = (jobject['msg'])
+    msg = msg.upper().rstrip()
+    botreply = str
 
-    if (msg== "HI" or msg=="HELLO" or msg=="HEY"):
-        botreply="Hello, please choose from one of the following: <br> 1. Send ExcelA <br>2. Send ExcelB"
-    elif (msg=="1"):
-        botreply="You selected option 1.<br>Thank you"
-    elif (msg=="2"):
-        botreply="You selected option 2.<br>Thank you"
+    if (msg == "HI" or msg == "HELLO" or msg == "HEY"):
+        return "Hello, please choose from one of the following: <br> 1. Send ExcelA <br>2. Send ExcelB "
+    elif (msg == "1"):
+        botreply="Received request for option 1. Thank you"
+        return "Email sent for option 1",sendemailmsg.sendemailmsg("1")
+    elif (msg == "2"):
+        botreply="Received response for option 2. Thank you"
+        return "Email sent for option 2",sendemailmsg.sendemailmsg("2")
     else:
-        botreply="I don't understand this. Try again :) <br>Please choose from one of the following: <br>1. Send ExcelA <br>2. Send ExcelB"
+        botreply = "I don't understand this. Try again :) <br>Please choose from one of the following: <br>1. Send ExcelA <br>2. Send ExcelB"
     return botreply
 
-    
+@app.route('/logout',methods=["GET"])
+def logout():
+    if 'loggedin' in session:
+        session.pop('loggedin', None)
+        return render_template('login.html')
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
-
